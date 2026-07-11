691fc94ef7dbd3312c211cc0482403fb7741774f
Maribel Amaguaña <maribel.amaguana@espe.edu.ec>
2026-07-10 22:23:15 -0500

    feat: agrega entidad inmutable ReservationEvent con copias defensivas

---
+++ src/main/java/com/espe/reservation_stream/ReservationStreamApplication.java
@@ -0,0 +1,13 @@
+package com.espe.reservation_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espe/reservation_stream/ReservationStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espe.reservation_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
